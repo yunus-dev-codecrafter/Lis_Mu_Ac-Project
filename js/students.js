6161e6fa9972920626b069c93b7f25614a09a09f
Yunus <yunusabdulhameed1@gmail.com>
2025-09-16 18:26:33 +0100

    optimization 005

--- js/students.js
+++ js/students.js
@@ -1,6 +1,16 @@
 const students = {
   "yunusabdulhameed1@gmail.com": "Students/Yunus.html",
   "auabdullahi67@gmail.com": "Students/Abubakar.html",
+  "usarmkk@yahoo.com": "Students/Usman.html",
+  "gimba.saleh84@gmail.com": "Students/Gimba.html",
+  "dumstsamiya@gmail.com": "Students/Fatima.html",
+  "aminamuhammad3013@gmail.com": "Students/Amina.html",
+  "bashirtoffa30@gmail.com": "Students/Bashir.html",
+  "layyatusalisu4949@gmail.com": "Students/Layytu.html",
+  "khaadijaabdullah@gmail.com": "Students/Khadija.html",
+  "fadilakashim01@gmail.com": "Students/Fadila.hmtl",
+  "nifu2005@gmail.com": "Students/Hanifa.html",
+  "faizaismailzoaka@gmail.com": "Students/Faiza.html",
 };
 
 function login(event) {
@@ -10,6 +20,16 @@ function login(event) {
   const students = {
     "yunusabdulhameed1@gmail.com": "Students/Yunus.html",
     "auabdullahi67@gmail.com": "Students/Abubakar.html",
+    "usarmkk@yahoo.com": "Students/Usman.html",
+    "gimba.saleh84@gmail.com": "Students/Gimba.html",
+    "dumstsamiya@gmail.com": "Students/Fatima.html",
+    "aminamuhammad3013@gmail.com": "Students/Amina.html",
+    "bashirtoffa30@gmail.com": "Students/Bashir.html",
+    "layyatusalisu4949@gmail.com": "Students/Layytu.html",
+    "khaadijaabdullah@gmail.com": "Students/Khadija.html",
+    "fadilakashim01@gmail.com": "Students/Fadila.hmtl",
+    "nifu2005@gmail.com": "Students/Hanifa.html",
+    "faizaismailzoaka@gmail.com": "Students/Faiza.html",
   };
 
   if (students[email]) {
