58027670151671eddb29ed4bc3d18a959485ea69
Yunus <yunusabdulhameed1@gmail.com>
2025-09-16 12:51:19 +0100

    student page and login initiation

---
+++ js/students.js
@@ -0,0 +1,20 @@
+const students = {
+  "yunusabdulhameed1@gmail.com": "Students/Yunus.html",
+};
+
+function login(event) {
+  event.preventDefault(); // prevent page reload
+
+  const email = document.getElementById("email").value.trim();
+  const students = {
+    "yunusabdulhameed1@gmail.com": "students/Yunus.html",
+  };
+
+  if (students[email]) {
+    // Redirect to their dashboard
+    window.location.href = students[email];
+  } else {
+    // Show error
+    document.getElementById("message").textContent = "Email not registered.";
+  }
+}
