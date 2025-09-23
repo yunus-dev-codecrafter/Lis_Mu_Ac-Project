fec39963dc43df53df163c65b89faaca2f701b53
Yunus <yunusabdulhameed1@gmail.com>
2025-09-23 18:56:53 +0100

    latest update

--- Students/Usman.js
+++ Students/Usman.js
@@ -18,8 +18,8 @@ const joinBtn = document.getElementById("joinBtn");
 
 // define allowed sessions: day (0=Sunday, 1=Monday,...,6=Saturday), start, end
 const sessions = [
-  { day: 2, start: "19:00", end: "19:30" },
-  { day: 5, start: "19:00", end: "19:30" },
+  { day: 6, start: "19:00", end: "19:30" },
+  { day: 4, start: "19:00", end: "19:30" },
 ];
 
 function checkSessionTime() {
